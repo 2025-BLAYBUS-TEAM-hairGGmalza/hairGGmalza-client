d7038fe6587862cc386e3eaf0493c6701995a72c
suzzang2 <subin030102@naver.com>
2025-02-14 20:18:24 +0900

    FEAT: 리뷰 페이지 중간 저장

--- src/app/designer/[id]/review/page.tsx
+++ src/app/designer/[id]/review/page.tsx
@@ -8,9 +8,39 @@ import styled from 'styled-components';
 const Reviewpage = () => {
    const params = useParams();
 
+   //리뷰카드 임시 더미데이터
+   const reviewData = [
+      {
+         id: 1,
+         name: '박수빈 디자이너',
+         address: '서울 강남구 압구정로79길',
+         category: '홍대/연남/합정',
+         score: 4.5,
+         review: '리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다',
+         date: '2021.10.10'
+      },
+      {
+         id: 2,
+         name: '박수빈 디자이너',
+         address: '서울 강남구 압구정로79길',
+         category: '홍대/연남/합정',
+         score: 4.5,
+         review: '리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다',
+         date: '2021.10.10'
+      },
+      {
+         id: 3,
+         name: '박수빈 디자이너',
+         address: '서울 강남구 압구정로79길',
+         category: '홍대/연남/합정',
+         score: 4.5,
+         review: '리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는 길게끝까지더보기없이계속들어간다리뷰가줄줄줄줄여기에들어가는데 여기에서는길게끝까지더보기없이계속들어간다',
+         date: '2021.10.10'
+      }
+   ]
+
    return (
       <Wrapper>
-         {/* <ReviewPageHeader>리뷰</ReviewPageHeader> */}
          <Header where='designer' />
 
          <ProfileWrapper>
@@ -34,6 +64,39 @@ const Reviewpage = () => {
                </BottomProfile>
             </ProfileContainer>
          </ProfileWrapper>
+
+         <ReviewsContainer>
+            <ReviewTitleContainer>
+               <ReviewNumber>145개</ReviewNumber>
+               <ReviewOrder>최근 등록 순</ReviewOrder>
+            </ReviewTitleContainer>
+            <ReviewContents>
+               {reviewData.map((review) => (
+                  <ReviewCard key={review.id}>
+                     <TopProfile>
+                        <ProfileImage />
+                        <NameAndAddress>  
+                           <Name>{review.name}</Name>
+                           <Address>
+                              <span id='address_detail' style={{marginRight:'10px'}}>{review.address}</span>
+                              <span id='address_category' style={{color: '#808080'}}>{review.category}</span>
+                           </Address>
+                        </NameAndAddress>
+                     </TopProfile>
+                     <RealReviewContainer>
+                        <ReviewScore>
+                           <ReviewScoreNumber>{review.score}</ReviewScoreNumber>
+                           <ReviewScoreStar>★★★★☆</ReviewScoreStar>
+                        </ReviewScore>
+                        <ReviewContent>
+                           <ReviewText>{review.review}</ReviewText>
+                           <ReviewDate>{review.date}</ReviewDate>
+                        </ReviewContent>
+                     </RealReviewContainer>
+                  </ReviewCard>
+               ))}
+            </ReviewContents>
+         </ReviewsContainer>
       </Wrapper>
    )
 }
@@ -49,16 +112,6 @@ const Wrapper = styled.div`
    align-items: center;
    justify-content: center;
    position: relative;
-`
-
-const ReviewPageHeader = styled.header`
-   width: 100%;
-   height: 55px;
-   display: flex;
-   align-items: center;
-   text-align: center;
-   justify-content: center;
-   font-size: 20px;
 `
 
 
@@ -159,3 +212,111 @@ const ScoreStar = styled.div`
    font-size: 35px;
    color: #000000;
 `
+
+
+/////////////////////
+const ReviewsContainer = styled.div`
+   width: 100%;
+   padding: 20px;
+   box-sizing: border-box;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   background-color: #f0f0f0;
+`
+
+const ReviewTitleContainer = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+`
+
+const ReviewNumber = styled.div`
+   font-size: 21px;
+   font-weight: bold;
+`
+
+const ReviewOrder = styled.div`
+   font-size: 13px;
+   color: #808080;
+   background-color: #f6f6f6;
+   padding: 5px 10px;
+   border-radius: 6px;
+`
+
+const ReviewContents = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   margin-top: 10px;
+`
+
+const ReviewCard = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   margin-top: 10px;
+   padding: 20px;
+   box-sizing: border-box;
+   border-radius: 6px;
+   background-color: #ffffff;
+   gap: 10px;
+`
+
+const RealReviewContainer = styled.div`
+   width: 100%;
+   padding: 20px;
+   box-sizing: border-box;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: space-between;
+   background-color: #f0f0f0;
+   gap: 10px;
+`
+
+const ReviewScore = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   justify-content: flex-start;
+   gap: 10px;
+`
+
+const ReviewScoreNumber = styled.div`
+   font-size: 18px;
+   font-weight: bold;
+`
+
+const ReviewScoreStar = styled.div`
+   font-size: 18px;
+`
+
+const ReviewContent = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+   justify-content: center;
+`
+
+const ReviewText = styled.div`
+   font-size: 15px;
+   margin-bottom: 10px;
+
+   //줄간격
+   line-height: 1.4;
+`
+
+const ReviewDate = styled.div`
+   font-size: 12px;
+   color: #808080;
+`
+
