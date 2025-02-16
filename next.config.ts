20a06cb2e7915653ce76a7948504f0505068b4b9
suzzang2 <subin030102@naver.com>
2025-02-16 12:45:21 +0900

    CHORE: 방금거 취소

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone", // Vercel 및 Netlify에서 SSR 및 API 핸들러 지원
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        destination: "https://haertz.vercel.app/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
