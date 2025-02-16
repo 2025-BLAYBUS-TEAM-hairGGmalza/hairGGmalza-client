2eeda3baf8be75ce67038b0e07ccd9bc81a5c650
suzzang2 <subin030102@naver.com>
2025-02-16 12:43:37 +0900

    CHORE: vercel 배포를 위한 redirects 수정 등

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Vercel 및 Netlify에서 SSR 및 API 핸들러 지원
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        destination: "https://haertz.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
