a50f78c95af2288bc5a899ce2a3c45d4fd35eb3e
CJY <cjy3458@naver.com>
2025-02-17 17:49:38 +0900

    FEAT: ProgressCarousel 컴포넌트 분리

--- src/components/main/Section1/Section1.tsx
+++ src/components/main/Section1/Section1.tsx
@@ -4,11 +4,18 @@ import React from "react";
 import MarqueeText from "../MarqueeText";
 import ProgressCarousel from "../../common/carousel/ProgressCarousel";
 
+const images = [
+  { src: "/images/Haertz Branding-10.png", alt: "Image 1" },
+  { src: "/images/Haertz Branding-11.png", alt: "Image 2" },
+  { src: "/images/Haertz Branding-12.png", alt: "Image 3" },
+  { src: "/images/Haertz Branding-13.png", alt: "Image 4" },
+];
+
 const Section1 = () => {
   return (
     <>
       <MarqueeText />
-      <ProgressCarousel />
+      <ProgressCarousel images={images} />
     </>
   );
 };
