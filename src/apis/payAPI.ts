408112ff8282e1d4b462a288dfcb9625a8d655bc
suzzang2 <subin030102@naver.com>
2025-02-20 16:12:08 +0900

    FIX: id 수정

--- src/apis/payAPI.ts
+++ src/apis/payAPI.ts
@@ -61,9 +61,8 @@ export const postReservation = async (
             console.log("✅ 계좌이체 성공");
             console.log(response.data);
             console.log(response.data.data);
-            console.log(response.data.data.reservationId);
             //결제 상세 내역 페이지로 이동
-            // window.location.href=`/reservation/${response.data.data.reservationId}`//
+            window.location.href=`/reservation/${response.data.data.id}`//
          }
       }
 
